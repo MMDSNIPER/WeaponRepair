a04a6cc8e331e68e223981c6da5286b745c3fa14
SNIPER <gametech.ir@gmail.com>
2022-05-08 11:30:53 +0430

    c:s/fix
    + ped
    + item check

--- client.lua
+++ client.lua
@@ -1,13 +1,11 @@
 local ox_inventory = exports.ox_inventory
-local weapons = {'WEAPON_ADVANCEDRIFLE','WEAPON_APPISTOL','WEAPON_ASSAULTRIFLE','WEAPON_ASSAULTSHOTGUN','WEAPON_BULLPUPRIFLE','WEAPON_CARBINERIFLE','WEAPON_COMBATMG','WEAPON_COMBATPISTOL','WEAPON_COMBATPDW','WEAPON_COMBATSHOTGUN','WEAPON_HEAVYSNIPER','WEAPON_HEAVYSNIPER_MK2','WEAPON_MARKSMANRIFLE','WEAPON_MARKSMANRIFLE_MK2','WEAPON_MG','WEAPON_MICROSMG','WEAPON_MINIGUN','WEAPON_MOLOTOV','WEAPON_MUSKET','WEAPON_PETROLCAN','WEAPON_PISTOL','WEAPON_PISTOL50','WEAPON_PROXMINE','WEAPON_PUMPSHOTGUN','WEAPON_REVOLVER','WEAPON_RPG','WEAPON_SAWNOFFSHOTGUN','WEAPON_SMG','WEAPON_SMG_MK2','WEAPON_SNIPERRIFLE','WEAPON_SNIPERRIFLE_MK2','WEAPON_SWITCHBLADE','WEAPON_VINTAGEPISTOL','WEAPON_ASSAULTRIFLE_MK2','WEAPON_ASSAULTSMG','WEAPON_BAT','WEAPON_BATTLEAXE','WEAPON_BULLPUPSHOTGUN','WEAPON_CARBINERIFLE_MK2','WEAPON_CERAMICPISTOL','WEAPON_COMBATMG_MK2','WEAPON_COMPACTRIFLE','WEAPON_CROWBAR','WEAPON_DAGGER','WEAPON_DBSHOTGUN','WEAPON_DOUBLEACTION','WEAPON_FIREWORK','WEAPON_FLAREGUN','WEAPON_FLASHLIGHT','WEAPON_GOLFCLUB','WEAPON_GUSENBERG','WEAPON_HAMMER','WEAPON_HATCHET','WEAPON_HEAVYPISTOL','WEAPON_HEAVYSHOTGUN','WEAPON_KNIFE','WEAPON_KNUCKLE','WEAPON_MACHETE','WEAPON_MACHINEPISTOL','WEAPON_MARKSMANPISTOL','WEAPON_MILITARYRIFLE','WEAPON_MINISMG','WEAPON_NAVYREVOLVER','WEAPON_NIGHTSTICK','WEAPON_PISTOL_MK2','WEAPON_POOLCUE','WEAPON_PUMPSHOTGUN_MK2','WEAPON_REVOLVER_MK2','WEAPON_SNSPISTOL','WEAPON_SNSPISTOL_MK2','WEAPON_SPECIALCARBINE','WEAPON_SPECIALCARBINE_MK2','WEAPON_STONE_HATCHET','WEAPON_STUNGUN','WEAPON_SWEEPERSHOTGUN','WEAPON_WRENCH'}
 
-local function openContext()
+function openContext()
     lib.registerContext({
         id = 'weapon_menu',
         title = 'Repair Weapon',
         options = {
             ['Do you wish to repair your weapon?'] = {
-                description = 'Yes',
                 event = 'WRepair:repairWeapon'
             }
         }
@@ -16,26 +14,10 @@ local function openContext()
 end
 
 RegisterNetEvent('WRepair:repairWeapon', function()
-    if true then
+    local rcount = ox_inventory:Search('count', 'repairkit_weapon')
+    if rcount > 0 then
         lib.callback('WRepair:Cost', false, function(response)
             if response then
-               -- Uncomment this if you plan on adding the item check back. *shrug*
-                --[[ 
-                lib.notify({
-                    title = 'Error',
-                    description = 'You are missing a Weapon Kit!',
-                    position = 'top',
-                    duration = 3500,
-                    style = {
-                        backgroundColor = 'lightcoral',
-                        color = 'white'
-                    },
-                    icon = 'gun',
-                    iconColor = 'white'
-                })
-                openContext()
-                --]]
-            elseif not response then
                 lib.notify({
                     title = 'Success',
                     description = 'You have repaired your weapon!',
@@ -48,33 +30,76 @@ RegisterNetEvent('WRepair:repairWeapon', function()
                     icon = 'gun',
                     iconColor = 'white'
                 })
+            else
+                lib.notify({
+                    title = 'Error',
+                    description = 'You have no weapons!',
+                    position = 'top',
+                    duration = 3500,
+                    style = {
+                        backgroundColor = 'lightcoral',
+                        color = 'white'
+                    },
+                    icon = 'gun',
+                    iconColor = 'white'
+                })
             end
         end)
+    else
+        lib.notify({
+            title = 'Error',
+            description = 'You are missing a Weapon Kit!',
+            position = 'top',
+            duration = 3500,
+            style = {
+                backgroundColor = 'lightcoral',
+                color = 'white'
+            },
+            icon = 'gun',
+            iconColor = 'white'
+        })
     end
 end)
 
-local repairguy = `a_f_m_prolhost_01`
-    exports["qtarget"]:AddTargetModel(repairguy, {
-        options = {
-            {
-                icon = "fa-solid fa-gun",
-                label = "Repair Weapon",
-                action = function(entity)
-                    openContext()
-                end
-            }
-        },
-    distance = 3.0
-})
+Config = {
+    PedModel = `cs_josef`,
+    Peds = {
+        vector4(12.0596, -1106.9465, 29.7970, 342.0751),
+    }
+}
 
--- Map blip
-CreateThread(function ()
-    blip = AddBlipForCoord(231.6, -1094.99) -- x & y coordinates
-    SetBlipSprite(blip, 110) --  icon 
-    SetBlipScale(blip, 0.7) -- size
-    SetBlipColour(blip, 0) -- color
-    SetBlipAsShortRange(blip, true)
-    BeginTextCommandSetBlipName('STRING')
-    AddTextComponentString('Repair Station') -- name that shows on the map
-    EndTextCommandSetBlipName(blip)
+-- Create Peds
+Citizen.CreateThread(function()
+    for i=1, #Config.Peds, 1 do
+        -- Blip
+        local blip = AddBlipForCoord(Config.Peds[i].x, Config.Peds[i].y)
+        SetBlipSprite(blip, 110)
+        SetBlipScale(blip, 0.7)
+        SetBlipColour(blip, 0)
+        SetBlipAsShortRange(blip, true)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentString('Repair Station')
+        EndTextCommandSetBlipName(blip)
+
+        -- Ped
+        RequestModel(Config.PedModel)
+        while not HasModelLoaded(Config.PedModel) do 
+            Citizen.Wait(1)
+        end
+        local ped = CreatePed(4, Config.PedModel, Config.Peds[i].x, Config.Peds[i].y, Config.Peds[i].z - 1, Config.Peds[i].w, false, true)
+        FreezeEntityPosition(ped, true)
+        SetEntityInvincible(ped, true)
+        exports.qtarget:AddTargetEntity(ped, {
+            options = {
+                {
+                    icon = "fa-solid fa-screwdriver-wrench",
+                    label = "Reapir Bench",
+                    action = function(entity)
+                        openContext()
+                    end
+                },
+            },
+            distance = 2
+        })
+    end
 end)
